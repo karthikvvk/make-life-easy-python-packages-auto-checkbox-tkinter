ba99c49122feeff64545637418c61639a6db8f79
karthik <130954788+karthikvvk@users.noreply.github.com>
2023-07-27 11:44:28 +0530

    Update automate_checkbox.py

--- make-life-easy-python-packages-auto-checkbox-tkinter/automate_checkbox.py
+++ make-life-easy-python-packages-auto-checkbox-tkinter/automate_checkbox.py
@@ -1,7 +1,8 @@
 """
-Create checkboxes using the given sequence and return the checkbox objects in 'vari'
+Creates checkboxes using the given sequence and return the checkbox objects in 'vari'
+Also can get the checked items using fetch_cked_val(); return as list
 That is 'print(cr_checkbox(tk_window_object, sequence)[1])' will print all the checkbox's objects.
-And 
+And
 
 for i in cr_checkbox(tk_window_object, sequence)[1]:
         print(i.get())
@@ -10,13 +11,17 @@ the above will print the value(1 or 0), that is the status of checked(1) or unch
 """
 from tkinter import *
 
+vari = []
+seq = []
 
-def cr_checkbox(tk_window_object, sequence, bg='white', fg='black', row=0, column=0):
+
+def cr_checkbox(tk_window_object, sequence, bg='white', fg='black', row_lb=0, column_lb=0):
+    global vari, seq
     len_seq = len(sequence)
-    vari = []
     row_l = []
+    seq = sequence
 
-    for y in range(row, len_seq + row):
+    for y in range(row_lb, len_seq + row_lb):
         row_l.append(y)
 
     for i in range(len_seq):
@@ -25,6 +30,17 @@ def cr_checkbox(tk_window_object, sequence, bg='white', fg='black', row=0, colum
 
     for i in range(len_seq):
         Checkbutton(tk_window_object, text=sequence[i], variable=vari[i], bg=bg, justify='left', fg=fg).grid(
-            row=row_l[i], column=column)
-    return [len_seq, vari, sequence]
-    
+            row=row_l[i], column=column_lb)
+
+    return vari
+
+
+def fetch_cked_val():
+    global vari, seq
+
+    bx_chked = []
+    for t in range(len(vari)):
+        if vari[t].get() == 1:
+            bx_chked.append(seq[t])
+
+    return bx_chked
