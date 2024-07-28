4c57c38c104ce89336c90c1516086bb0c032ca52
karthik <vkarthik5002@gmail.com>
2024-07-28 19:26:16 +0530

    Update automate_checkbox.py

--- make-life-easy-python-packages-auto-checkbox-tkinter/automate_checkbox.py
+++ make-life-easy-python-packages-auto-checkbox-tkinter/automate_checkbox.py
@@ -1,82 +1,97 @@
-"""
-cr_checkbox(): takes the tkinter window object and the items to be listed as arguments and return checkbox objects as list.
-fetch_cked_val(): takes the list returned by cr_checkbox() as argument and return a list of checked/selected items.
-**NOTE: ONLY WORK FOR TKINTER.
-    >Default  layout managers is .grid()
-    >Please change according  to your prefered layout managers(.place(), .pack(), etc.).
-"""
 from tkinter import *
+from collections import OrderedDict
+ 
+class automate_checkbox:
+    vari = OrderedDict()
+    seq = []
+    obj = OrderedDict()
+    len_seq = 0
+    root = 0
+    starting_row = 0
+    starting_col = 0
+    
+    def __init__(self, window) -> None:
+        self.root = window
+        
+    def cr_checkbox(self, sequence, bg='white', fg='black', row_lb=0, column_lb=0):
+        self.len_seq = len(sequence)
+        self.seq = sequence
+        self.starting_row = row_lb
+        self.starting_col = column_lb
+        row_l = []
+        for y in range(row_lb, self.len_seq + row_lb):
+            row_l.append(y)
 
-vari = []
-seq = []
-obu = []
-len_seq = 0
+        for i in range(self.len_seq):
+            it = self.seq[i]
+            self.vari[it] = BooleanVar(self.root, value=False)
+            self.obj[it] = Checkbutton(self.root, text=it, variable=self.vari[it], bg=bg, justify='left', fg=fg)
+            self.obj[it].grid(row=row_l[i], column=column_lb)
+        
+    def fetch_cked_val(self):
+        lis = []
+        for i in self.seq:
+            if self.vari[i].get():
+               lis.append(i) 
+        return lis
+    
+    def remove_box(self, element=[], remove_all=False):
+        if remove_all:
+            for i in self.seq:
+                self.obj[i].destroy()
+                del self.obj[i]
 
+                del self.vari[i]
 
-def cr_checkbox(tk_window_object, sequence, bg='white', fg='black', row_lb=0, column_lb=0):
-    global vari, seq, obu, len_seq
-    vari, row_l = [], []
-    len_seq = len(sequence)
-    seq = sequence
+                self.seq.remove(i)
+                self.len_seq = 0
+        elif element:
+            le = len(element)
+            for i in element:
+                self.obj[i].destroy()
+                del self.obj[i]
 
-    for y in range(row_lb, len_seq + row_lb):
-        row_l.append(y)
+                del self.vari[i]
 
-    for i in range(len_seq):
-        vari.append('a' + str(i))
-        obu.append('a' + str(i))
-        vari[i] = BooleanVar(tk_window_object, value=False)
-        obu[i] = Checkbutton(tk_window_object, text=sequence[i], variable=vari[i], bg=bg, justify='left', fg=fg)
-        obu[i].grid(row=row_l[i], column=column_lb)
+                self.seq.remove(i)
+            self.len_seq -= le
+        else:
+            ele = self.seq[-1]
+            self.obj[ele].destroy()
+            del self.obj[ele]
 
-    return [vari, obu]
+            del self.vari[ele]
 
+            self.seq.remove(ele)
+            self.len_seq -= 1
 
-def fetch_cked_val(chbx_obj):
-    global seq
+        for i in range(self.len_seq):
+            it = self.seq[i]
+            self.obj[it].grid(row=self.starting_row+i, column=self.starting_col)
+        
+    def add_box(self, squence=[], bg='white', fg='black', rlb=starting_row, column_lb=starting_col):
+        ln = len(squence)
+        sq = squence
+        variables, objts = OrderedDict(), OrderedDict()
+        
+        for i in range(ln):
+            it = sq[i]
+            variables[it] = BooleanVar(self.root, value=False)
+            objts[it] = Checkbutton(self.root, text=it, textvariable=variables[it], bg=bg, justify='left', fg=fg)
 
-    bx_chked = []
-    for t in range(len(seq)):
-        if chbx_obj[t].get():
-            bx_chked.append(seq[t])
+        org_dict = list(self.obj.items())
+        new_dict = list(objts.items())
+        org_vari_dict = list(self.vari.items())
+        new_vari_dict = list(variables.items())
+        for i in range(ln):
+            self.seq.insert(rlb+i, squence[i])
+            org_dict.insert(rlb+i, new_dict[i])
+            org_vari_dict.insert(rlb+i, new_vari_dict[i])
+        
+        self.len_seq = len(self.seq)
+        self.obj = OrderedDict(org_dict)
+        self.vari = OrderedDict(org_vari_dict)
 
-    return bx_chked
-
-
-def remove(element=[], remove_all=False):
-    global obu, seq, vari
-    if element:
-        for i in element:
-            ind = seq.index(element[i])
-            obu[ind].destroy()
-            obu.pop(ind)
-    else:
-        obu[-1].destroy()
-        obu.pop()
-    if remove_all:
-        for i in obu:
-            i.destroy()
-            obu = []
-
-
-def add(tk_window_object, row_lb=0, elements=[], bg='white', fg='black', column_lb=0):
-    global obu, seq, vari, len_seq
-    if row_lb > len_seq:
-        pass
-    else:
-        row_lb = len_seq
-    row_l = []
-
-    add_len_seq = len(elements)
-    seq += elements
-
-    for y in range(row_lb, add_len_seq + row_lb+1):
-        row_l.append(y)
-
-    for i in range(len_seq, len_seq+add_len_seq):
-        vari.append('a' + str(i))
-        obu.append('a' + str(i))
-        vari[i] = BooleanVar(tk_window_object, value=False)
-        obu[i] = Checkbutton(tk_window_object, text=elements[len_seq-i], variable=vari[i], bg=bg, justify='left', fg=fg)
-        obu[i].grid(row=i, column=column_lb)
-    len_seq = len_seq+add_len_seq
+        for i in range(self.len_seq):
+            it = self.seq[i]
+            self.obj[it].grid(row=rlb+i, column=column_lb)
